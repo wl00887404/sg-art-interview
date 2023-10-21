80f6774d3ca3d6a9095793d6de255a44236cd823
Max He <wl00887404@gmail.com>
2023-10-21 23:02:30 +0800

    :recycle: Move <App> to <TodoList>

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,43 +1,7 @@
-import styled from 'styled-components';
-
-import Input from './components/shared/Input';
-import TodoList from './components/TodoList/TodoList';
-import typography from './components/shared/typography';
-
-const Container = styled.div`
-  padding: 2.5rem;
-  max-width: 33.75rem;
-  margin: 0 auto;
-`;
-
-const Title = styled.h1`
-  ${typography.heading}
-  color: var(--white);
-  text-align: center;
-  margin-bottom: 1.375rem;
-`;
-
-const InputContainer = styled.div`
-  margin-bottom: 1.625rem;
-`;
-
-const TodoListContainer = styled.div`
-  width: 26.875rem;
-  margin: 0 auto;
-`;
+import TodoList from './pages/TodoList';
 
 function App() {
-  return (
-    <Container>
-      <Title>TODO</Title>
-      <InputContainer>
-        <Input placeholder="輸入代辦事項" />
-      </InputContainer>
-      <TodoListContainer>
-        <TodoList />
-      </TodoListContainer>
-    </Container>
-  );
+  return <TodoList />;
 }
 
 export default App;
---
+++ client/src/pages/TodoList.tsx
@@ -0,0 +1,43 @@
+import styled from 'styled-components';
+
+import Input from '../components/shared/Input';
+import TodoList from '../components/TodoList/TodoList';
+import typography from '../components/shared/typography';
+
+const Container = styled.div`
+  padding: 2.5rem;
+  max-width: 33.75rem;
+  margin: 0 auto;
+`;
+
+const Title = styled.h1`
+  ${typography.heading}
+  color: var(--white);
+  text-align: center;
+  margin-bottom: 1.375rem;
+`;
+
+const InputContainer = styled.div`
+  margin-bottom: 1.625rem;
+`;
+
+const TodoListContainer = styled.div`
+  width: 26.875rem;
+  margin: 0 auto;
+`;
+
+function TodoListPage() {
+  return (
+    <Container>
+      <Title>TODO</Title>
+      <InputContainer>
+        <Input placeholder="輸入代辦事項" />
+      </InputContainer>
+      <TodoListContainer>
+        <TodoList />
+      </TodoListContainer>
+    </Container>
+  );
+}
+
+export default TodoListPage;
