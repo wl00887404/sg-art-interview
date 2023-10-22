af6e719a64ae30921692b117cd053ee0a1ced745
Max He <wl00887404@gmail.com>
2023-10-22 19:59:20 +0800

    :heavy_plus_sign: Add react-router-dom and axios

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,26 @@
-// import TodoList from './pages/TodoList';
-// import LoginDialog from './pages/LoginDialog';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import TodoList from './pages/TodoList';
+import LoginDialog from './pages/LoginDialog';
 import RegisterDialog from './pages/RegisterDialog';
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <TodoList />,
+  },
+  {
+    path: '/login',
+    element: <LoginDialog />,
+  },
+  {
+    path: '/register',
+    element: <RegisterDialog />,
+  },
+]);
+
 function App() {
-  return <RegisterDialog />;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
