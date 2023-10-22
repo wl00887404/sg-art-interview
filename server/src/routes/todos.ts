8a92321c4c6da6c0cfe2e73a9d64de05f4644c66
Max He <wl00887404@gmail.com>
2023-10-23 02:43:16 +0800

    :sparkles: Change todos api response

--- server/src/routes/todos.ts
+++ server/src/routes/todos.ts
@@ -28,12 +28,15 @@ todosRouter.post('/', (req, res) => {
   const { id, finished } = req.body;
 
   const todo = user!.todos.find((todo: any) => todo.id === id);
+  if (todo == null) {
+    return res.json(createFailedResponse(`找不到 id 為 ${id} 的代辦事項`));
+  }
   todo!.finished = finished;
   user!.save();
 
   return res.json(
     createSuccessResponse({
-      todos: user!.todos,
+      todo,
     }),
   );
 });
@@ -42,12 +45,12 @@ todosRouter.put('/', (req, res) => {
   const { user } = req;
   const { content } = req.body;
 
-  user!.todos.push({ content, finished: false });
+  user!.todos.unshift({ content, finished: false });
   user!.save();
 
   return res.json(
     createSuccessResponse({
-      todos: user!.todos,
+      todo: user!.todos[0],
     }),
   );
 });
@@ -56,12 +59,18 @@ todosRouter.delete('/', (req, res) => {
   const { user } = req;
   const { id } = req.query;
 
+  const deletedTodo = user!.todos.find(todo => todo.id === id);
+
+  if (deletedTodo == null) {
+    return res.json(createFailedResponse(`找不到 id 為 ${id} 的代辦事項`));
+  }
+
   user!.todos = user!.todos.filter(todo => todo.id !== id);
   user!.save();
 
   return res.json(
     createSuccessResponse({
-      todos: user!.todos,
+      todo: deletedTodo,
     }),
   );
 });
